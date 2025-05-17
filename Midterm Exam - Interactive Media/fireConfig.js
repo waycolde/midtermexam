d4165f3f8a6a99d154576739e7de3d27bea2b970
waycolde <xeno.zyrene@gmail.com>
2025-05-17 10:46:17 +0800

    Add files via upload

---
+++ Midterm Exam - Interactive Media/fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBhzuY-lSmw9UTwNtiSKyumRWeqqXWhpRQ",
+  authDomain: "login-9a928.firebaseapp.com",
+  databaseURL: "https://login-9a928-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "login-9a928",
+  storageBucket: "login-9a928.firebasestorage.app",
+  messagingSenderId: "1014563820699",
+  appId: "1:1014563820699:web:7c60e66913beb66b5e623e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig); 
